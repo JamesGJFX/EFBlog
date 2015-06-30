37bc01a83f5061003a6af827d91c1145a2350580
James Farrell <JamesGJFX@gmail.com>
2015-06-30 13:23:09 +0100

    Adding skeleton MVC site

---
+++ Blog2/DAL/BlogContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Blog2.Models;
+
+namespace Blog2.DAL
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvection>();
+        }
+    }
+}
---
+++ Blog2/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog2.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string PostTitle { get; set; }
+        public string PostBody { get; set; }
+        public DateTime PostDate { get; set; }
+
+        public virtual ICollection<Tag> Tags { get; set; }
+    }
+}
